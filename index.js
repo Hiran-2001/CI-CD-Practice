a422cc9f7ea579bbfa3c19be50a8313c8d5e8240
Hiran Raj <hirankvlr@gmail.com>
2024-10-13 22:02:04 +0530

    new commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000;
 
 console.log('Server First hit')
 
-app.get('/', (req, res) => {
+app.get('/get', (req, res) => {
    res.send('Hello World from Node.js API');
 });
 
